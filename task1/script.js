d671b4c7ace4a7de674333e3bd0db194826a50bd
Valentin Vetukov <finik001@gmail.com>
2022-05-14 15:53:58 +0300

    Change style and add actions in ex 6.8.1

--- task1/script.js
+++ task1/script.js
@@ -1,1 +1,8 @@
+let counterClick = 0;
+
 console.log('Привет, мир!');
+
+document.querySelector('p').addEventListener('click', () => {
+    console.clear();
+    console.log(`Вы кликнули по параграфу ${++counterClick} раз`);
+})
